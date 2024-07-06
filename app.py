be7f7aa9c016cf7d7f76feb22c92c62b53ad2c87
nooralight <2017100000041@seu.edu.bd>
2024-07-06 08:19:41 +0600

    update

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ def bookings():
                      "20:30 - 22:00":{"Pádel 1":None,"Pádel 2":None,"Pádel 3":None,"Pádel 4":None,"Pádel 5":None,"Pádel 6":None,}}
     
     if today_bookings.count() == 0:
-        return render_template("bookings.html", t_date = date_str, booking_dict = booking_dict)
+        return render_template("bookings.html", t_date = "2024-07-04", booking_dict = booking_dict)
     
     else:
         for booking in today_bookings:
