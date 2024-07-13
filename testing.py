a56e9af7e76a83640e9eb41aee36a9d0ac8582a0
nooralight <2017100000041@seu.edu.bd>
2024-07-14 01:38:34 +0600

    upload

--- testing.py
+++ testing.py
@@ -58,7 +58,7 @@ iframe_soup = BeautifulSoup(iframe_page_source, 'html.parser')
 
 # Extract the div elements with class 'ui-selectable' inside 'contenedor'
 reservas_container = iframe_soup.find('div', {'class': 'myReservas'}).find('div', {'id': 'contenedor'})
-selectable_divs = reservas_container.find('div', {'class': 'ui-selectable'})
+selectable_divs = reservas_container.find_all('div', {'class': 'ui-selectable'})
 another_selectable_divs = selectable_divs.find_all('div', {'class':'col01 ui-selectee'})
 each_routine = another_selectable_divs.find_all('div', {'class':'imanEvento ui-draggable ui-draggable-handle'})
 
