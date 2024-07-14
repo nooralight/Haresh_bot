b4393eb7820ba1d2918553a06a0781660b7bb79f
nooralight <2017100000041@seu.edu.bd>
2024-07-14 16:19:29 +0600

    update

--- testing.py
+++ testing.py
@@ -42,86 +42,96 @@ WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.TAG_NAME, "bo
 
 # Give additional time for page elements to load if necessary
 time.sleep(40)
+
 # Switch to the iframe
 iframe = driver.find_element(By.ID, "iframeContenido")
 driver.switch_to.frame(iframe)
 
-# Wait for the iframe content to load
-WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, "CuerpoTabla")))
+for i in range(7):
+    if i > 0:
+        # Wait for the iframe content to load
+        WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, "ctl01_CC_ImageButtonAvanzarFechaDrch")))
+        # Click the login button
+        next_button = driver.find_element(By.ID, "ctl01_CC_ImageButtonAvanzarFechaDrch")
+        next_button.click()
 
-# Get the page source of the iframe content
-iframe_page_source = driver.page_source
+
+    # Wait for the iframe content to load
+    WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, "CuerpoTabla")))
+
+    # Get the page source of the iframe content
+    iframe_page_source = driver.page_source
 
 
-# Parse the iframe content with BeautifulSoup
-iframe_soup = BeautifulSoup(iframe_page_source, 'html.parser')
+    # Parse the iframe content with BeautifulSoup
+    iframe_soup = BeautifulSoup(iframe_page_source, 'html.parser')
 
-# Extract the div elements with class 'ui-selectable' inside 'contenedor'
-reservas_container = iframe_soup.find('div', {'class': 'myReservas'}).find('div', {'id': 'contenedor'})
-selectable_divs = reservas_container.find('div', {'class': 'ui-selectable'})
+    # Extract the div elements with class 'ui-selectable' inside 'contenedor'
+    reservas_container = iframe_soup.find('div', {'class': 'myReservas'}).find('div', {'id': 'contenedor'})
+    selectable_divs = reservas_container.find('div', {'class': 'ui-selectable'})
 
-another_selectable_divs = selectable_divs.find_all('div', {'id':'selectable'})
+    another_selectable_divs = selectable_divs.find_all('div', {'id':'selectable'})
 
-for ext_div in another_selectable_divs:
-    each_routine = ext_div.find_all('div', {'class': 'imanEvento ui-draggable ui-draggable-handle'})
+    for ext_div in another_selectable_divs:
+        each_routine = ext_div.find_all('div', {'class': 'imanEvento ui-draggable ui-draggable-handle'})
 
-    for div in each_routine:
-        evento_div = div.find('div', {'class': 'evento cursorNormal'})
-        booking_text = evento_div.find('div', {'class':'eventoSuperior'})
-        if evento_div:
+        for div in each_routine:
+            evento_div = div.find('div', {'class': 'evento cursorNormal'})
+            booking_text = evento_div.find('div', {'class':'eventoSuperior'})
+            if evento_div:
 
-            evento_id = evento_div.get('id')
-            evento_columna = evento_div.get('columna')
-            evento_style = evento_div.get('style')
-            pedal_dict = {"1": "Pádel 1", "2": "Pádel 2", "3":"Pádel 3", "4": "Pádel 4", "5":"Pádel 5", "6":"Pádel 6"}
-            state = "unknown"
-            if "rgb(249, 231, 182)" in evento_style:
-                state = "Searching"
-            elif "rgb(238, 164, 164)" in evento_style:
-                state = "Open"
-        
-            # event_info = f"ID: {evento_id}, Columna: {evento_columna}, Style: {evento_style}\n"
-            print("******   New Booking   ******")
-            print(f"Match number: {evento_id}\nCourt: {pedal_dict[evento_columna]}\nStatus: {state}")
-            if state!= "unknown":
-                names = []
-                for br in booking_text.find_all('br'):
-                    br.replace_with('\n')
-                # Extract text from each child element within the div
-                text_list = [element.get_text(separator='\n', strip=True) for element in booking_text]
-                serial = 0
-                for item in text_list:
-                    if item.startswith('Partida'):
-                        del text_list[serial]
-                    serial+= 1
-                # Join the text list with newline characters
-                # joined_text = "\n".join(text_list)
-                first_line = text_list[0].strip().split(" ")
-                timetable = first_line[0]
-                player_count = first_line[-1]
-                players_lines = []
-                for item in text_list[1:]:
-                    if '\n' in item:
-                        
-                        players_lines = item.split("\n")
-                    else:
-                        players_lines.append(item)
-                
-                print(f"Time period: {timetable}")
-                print(f"Player capacity: {player_count}")
-                print("Players")
-                print(players_lines)
-                # is_name_list = booking_text.find('span',{"class":"eventoTexto2"})
-                # print("Players")
-                # if is_name_list:
-                #     print(is_name_list.prettify)
-                # print()
-            else:
-                # # Join the text list with newline characters
-                # joined_text = "\n".join(text_list)
-                # print(joined_text)
-                print("Not a real booking")
-                print()
+                evento_id = evento_div.get('id')
+                evento_columna = evento_div.get('columna')
+                evento_style = evento_div.get('style')
+                pedal_dict = {"1": "Pádel 1", "2": "Pádel 2", "3":"Pádel 3", "4": "Pádel 4", "5":"Pádel 5", "6":"Pádel 6"}
+                state = "unknown"
+                if "rgb(249, 231, 182)" in evento_style:
+                    state = "Searching"
+                elif "rgb(238, 164, 164)" in evento_style:
+                    state = "Open"
+            
+                # event_info = f"ID: {evento_id}, Columna: {evento_columna}, Style: {evento_style}\n"
+                print("******   New Booking   ******")
+                print(f"Match number: {evento_id}\nCourt: {pedal_dict[evento_columna]}\nStatus: {state}")
+                if state!= "unknown":
+                    names = []
+                    for br in booking_text.find_all('br'):
+                        br.replace_with('\n')
+                    # Extract text from each child element within the div
+                    text_list = [element.get_text(separator='\n', strip=True) for element in booking_text]
+                    serial = 0
+                    for item in text_list:
+                        if item.startswith('Partida'):
+                            del text_list[serial]
+                        serial+= 1
+                    # Join the text list with newline characters
+                    # joined_text = "\n".join(text_list)
+                    first_line = text_list[0].strip().split(" ")
+                    timetable = first_line[0]
+                    player_count = first_line[-1]
+                    players_lines = []
+                    for item in text_list[1:]:
+                        if '\n' in item:
+                            
+                            players_lines = item.split("\n")
+                        else:
+                            players_lines.append(item)
+                    
+                    print(f"Time period: {timetable}")
+                    print(f"Player capacity: {player_count}")
+                    print("Players")
+                    print(players_lines)
+                    # is_name_list = booking_text.find('span',{"class":"eventoTexto2"})
+                    # print("Players")
+                    # if is_name_list:
+                    #     print(is_name_list.prettify)
+                    # print()
+                else:
+                    # # Join the text list with newline characters
+                    # joined_text = "\n".join(text_list)
+                    # print(joined_text)
+                    print("Not a real booking")
+                    print()
 
 
 
