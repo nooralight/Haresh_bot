eac648efac167f6a9de3400d8eab1e77f4c5db39
nooralight <2017100000041@seu.edu.bd>
2024-07-15 04:11:31 +0600

    update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from mongoengine import *
 from datetime import datetime,timedelta
 from db_player import add_new_player, update_player
 from db_booking import insert_new_booking, fetch_all_bookings_by_date, fetch_booking_by_id, get_numOfBookings, get_numOfunfinishedBookings
-
+import pytz
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -106,7 +106,9 @@ def home():
 
 @app.route('/bookings', methods=['POST','GET'])
 def bookings():
-    today_date = datetime.now()
+    # Define the timezone for Spain
+    spain_tz = pytz.timezone('Europe/Madrid')
+    today_date = datetime.now(spain_tz)
     date_str = today_date.strftime('%Y-%m-%d')
 
     today_bookings = fetch_all_bookings_by_date(date_str)
